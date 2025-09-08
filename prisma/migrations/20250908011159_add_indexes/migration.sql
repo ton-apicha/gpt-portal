88ce4f566a643ba3f865532cacc082fe045bf421
Ton Apicha <apicha.atsawa@gmail.com>
2025-09-08 08:12:06 +0700

    perf(db): add indexes Chat(userId,updatedAt) and Message(chatId,createdAt) via migration

---
+++ prisma/migrations/20250908011159_add_indexes/migration.sql
@@ -0,0 +1,5 @@
+-- CreateIndex
+CREATE INDEX "Chat_userId_updatedAt_idx" ON "Chat"("userId", "updatedAt");
+
+-- CreateIndex
+CREATE INDEX "Message_chatId_createdAt_idx" ON "Message"("chatId", "createdAt");
